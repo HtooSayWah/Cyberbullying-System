6ff0ad86771dc249541483d670d860b2481b7958
Thuya Myo Nyunt <ty@kny.co>
2018-12-10 22:14:02 +0630

    Add table create sql

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 #!/usr/bin/env python
-import os.path
+import os
 import tornado.escape
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
+import torndb
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+
+CRATE_TABLE = """
+CREATE TABLE IF NOT EXISTS posts (
+    id integer NOT NULL,
+    user_id character varying,
+    user_name character varying,
+    user_pic character varying,
+    post_time timestamp without time zone,
+    is_bullying boolean DEFAULT false,
+    bullying_words character varying[] DEFAULT [],
+);
+
+
+CREATE SEQUENCE IF NOT EXISTS posts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE posts_id_seq OWNED BY posts.id;
+"""
 
 # import and define tornado-y things
 from tornado.options import define
@@ -15,19 +43,49 @@ define("port", default=5000, help="run on the given port", type=int)
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-            (r"/([^/]+)?", MainHandler)
+            (r"/", MainHandler),
+            (r"/post", PostHandler)
+            (r"/feed", FeedHandler)
         ]
-        # settings = dict(
-        #     template_path=os.path.join(os.path.dirname(__file__), "templates"),
-        #     static_path=os.path.join(os.path.dirname(__file__), "static"),
-        #     debug=True,
-        # )
         tornado.web.Application.__init__(self, handlers)
 
 # the main page
 class MainHandler(tornado.web.RequestHandler):
     def get(self, q):
         self.write("Hello, world")
+
+'''
+Submit API
+{
+  "loginId": "String",
+  "username": "String",
+  "post": "String",
+  "timestamp": "Number"
+}
+'''
+
+# post manager
+class PostHandler(tornado.web.RequestHandler):
+    # TODO: Toei
+    def classifier(context):
+        return []
+
+    def checkBullingCount():
+
+    def post(self):
+        try:
+            data = tornado.escape.json_decode(self.request.body)
+
+            message = data["message"]
+            parseCommandFrom(message)
+        except Exception as e:
+            print e.message
+        self.write("This is post");
+
+class FeedHandler(tornado.web.RequestHandler):
+    def get(self, q):
+        # TODO: get from database and return 
+        # self.write("Hello, world");
 
 # RAMMING SPEEEEEEED!
 def main():

f8cb2615e68bdddbb2662e7d61195d5b8190aa9b
Thuya Myo Nyunt <ty@kny.co>
2018-12-10 21:19:42 +0630

    Init Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Cyber bullying detection system
+===============================
---
+++ main.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import os.path
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+# import and define tornado-y things
+from tornado.options import define
+define("port", default=5000, help="run on the given port", type=int)
+
+
+# application settings and handle mapping info
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/([^/]+)?", MainHandler)
+        ]
+        # settings = dict(
+        #     template_path=os.path.join(os.path.dirname(__file__), "templates"),
+        #     static_path=os.path.join(os.path.dirname(__file__), "static"),
+        #     debug=True,
+        # )
+        tornado.web.Application.__init__(self, handlers)
+
+# the main page
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, q):
+        self.write("Hello, world")
+
+# RAMMING SPEEEEEEED!
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(tornado.options.options.port)
+
+    # start it up
+    tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()

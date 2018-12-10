8cea672cb0b20863574789a66e7ada2b0efd45f7
Thuya Myo Nyunt <ty@kny.co>
2018-12-11 01:34:20 +0630

    Add database, loginHandler and Schema

--- main.py
+++ main.py
@@ -1,100 +1,110 @@
 #!/usr/bin/env python
+
 import os
-import tornado.escape
-import tornado.httpserver
-import tornado.ioloop
-import tornado.options
-import tornado.web
-import torndb
 import psycopg2
+import momoko
+from tornado import gen, web, auth, escape
+from tornado.ioloop import IOLoop
+from tornado.httpserver import HTTPServer
+from tornado.httpclient import AsyncHTTPClient
+import tornado.options as Options
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+Options.define("port", default=5000, help="run on the given port", type=int)
 
+class HomeHandler(web.RequestHandler):
+    def get(self):
+        self.write('Cyber Bullying Detection System!')
+        self.finish()
 
-CRATE_TABLE = """
-CREATE TABLE IF NOT EXISTS posts (
-    id integer NOT NULL,
-    user_id character varying,
-    user_name character varying,
-    user_pic character varying,
-    post_time timestamp without time zone,
-    is_bullying boolean DEFAULT false,
-    bullying_words character varying[] DEFAULT [],
-);
+DATABASE_URL = os.getenv('DATABASE_URL', 'dbname=toei_test host=localhost port=5432')
+FACEBOOK_APP_ID = os.getenv('FACEBOOK_APP_ID')
+FACEBOOK_APP_SECRET = os.getenv('FACEBOOK_APP_SECRET')
 
-
-CREATE SEQUENCE IF NOT EXISTS posts_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-ALTER SEQUENCE posts_id_seq OWNED BY posts.id;
-"""
-
-# import and define tornado-y things
-from tornado.options import define
-define("port", default=5000, help="run on the given port", type=int)
+USER_CREATE_QUERY = ("INSERT INTO users (facebook_id, user_name, user_pic, access_token, banned) "
+    "VALUES ('{facebook_id}', '{user_name}', '{user_pic}', '{access_token}', '{banned}')")
 
 
-# application settings and handle mapping info
-class Application(tornado.web.Application):
-    def __init__(self):
-        handlers = [
-            (r"/", MainHandler),
-            (r"/post", PostHandler)
-            (r"/feed", FeedHandler)
-        ]
-        tornado.web.Application.__init__(self, handlers)
+class LoginHandler(web.RequestHandler, auth.FacebookGraphMixin):
+    def initialize(self, database):
+        self.database = database
 
-# the main page
-class MainHandler(tornado.web.RequestHandler):
-    def get(self, q):
-        self.write("Hello, world")
+    async def create_user(self, user):
+        return await self.database.execute(
+            USER_CREATE_QUERY.format(
+                facebook_id=user['facebook_id'],
+                user_name=user['user_name'],
+                user_pic=user['user_pic'],
+                access_token=user['access_token'],
+                banned=user['banned'],
+            ))
+        
+    async def find_user_by_facebook_id(self, facebook_id):
+        cursor = await self.database.execute("SELECT * FROM users WHERE facebook_id = '{}'".format(facebook_id))
+        return cursor.fetchone()
 
-'''
-Submit API
-{
-  "loginId": "String",
-  "username": "String",
-  "post": "String",
-  "timestamp": "Number"
-}
-'''
+    async def fetch_facebook_user(self, user_access_token):
+        print('Call facebook')
+        response = await AsyncHTTPClient().fetch('https://graph.facebook.com'
+            '/me?fields=id,name,picture&access_token={}'.format(user_access_token))
+        return escape.json_decode(response.body)
 
-# post manager
-class PostHandler(tornado.web.RequestHandler):
-    # TODO: Toei
-    def classifier(context):
-        return []
-
-    def checkBullingCount():
-
-    def post(self):
+    async def post(self):
         try:
-            data = tornado.escape.json_decode(self.request.body)
+            body = escape.json_decode(self.request.body)
+            facebook_id = body['facebook_id']
+            user_access_token = body['access_token']
 
-            message = data["message"]
-            parseCommandFrom(message)
-        except Exception as e:
-            print e.message
-        self.write("This is post");
+            if not facebook_id or not user_access_token:
+                self.set_status(400)
 
-class FeedHandler(tornado.web.RequestHandler):
-    def get(self, q):
-        # TODO: get from database and return 
-        # self.write("Hello, world");
+            user = await self.find_user_by_facebook_id(facebook_id)
 
-# RAMMING SPEEEEEEED!
-def main():
-    tornado.options.parse_command_line()
-    http_server = tornado.httpserver.HTTPServer(Application())
-    http_server.listen(tornado.options.options.port)
+            if not user:
+                fb_user = await self.fetch_facebook_user(user_access_token)
+                if not fb_user:
+                    self.set_status(400)
+                    self.write('Invalid Request')
+                else:
+                    user = await self.create_user({
+                        'facebook_id': facebook_id,
+                        'user_name': fb_user['name'],
+                        'user_pic': fb_user['picture']['data']['url'],
+                        'access_token': user_access_token,
+                        'banned': False,
+                    })
+            else:
+                self.write({'success': True})
+        except (psycopg2.Warning, psycopg2.Error) as e:
+            self.set_status(400)
+            self.write({'success': False})
+            print(str(e))
 
-    # start it up
-    tornado.ioloop.IOLoop.instance().start()
+        self.finish()
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+    Options.parse_command_line()
+    ioloop = IOLoop.instance()
+
+    database = momoko.Pool(
+        dsn=DATABASE_URL,
+        size=1,
+        ioloop=ioloop,
+    )
+    application = web.Application([
+        (r'/', HomeHandler),
+        (r'/login', LoginHandler, dict(database=database)),
+        (r'/register', LoginHandler, dict(database=database)),
+    ], debug=True)
+
+
+    application.db = database
+    # this is a one way to run ioloop in sync
+    future = database.connect()
+    ioloop.add_future(future, lambda f: ioloop.stop())
+    ioloop.start()
+    future.result()  # raises exception on connection error
+
+    http_server = HTTPServer(application)
+    http_server.listen(Options.options.port)
+    ioloop.start()
---
+++ schema.sql
@@ -0,0 +1,41 @@
+DROP TABLE users;
+DROP TABLE posts;
+
+CREATE TABLE IF NOT EXISTS users (
+    id integer NOT NULL,
+    facebook_id character varying,
+    user_name character varying,
+    user_pic character varying,
+    access_token character varying,
+    banned boolean DEFAULT false
+);
+
+CREATE SEQUENCE IF NOT EXISTS users_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE users_id_seq OWNED BY users.id;
+ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
+
+CREATE TABLE IF NOT EXISTS posts (
+    id integer NOT NULL,
+    user_id character varying,
+    user_name character varying,
+    user_pic character varying,
+    post_time timestamp without time zone,
+    is_bullying boolean DEFAULT false,
+    bullying_words character varying[] DEFAULT array[]::varchar[]
+);
+
+CREATE SEQUENCE IF NOT EXISTS posts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+ALTER SEQUENCE posts_id_seq OWNED BY posts.id;
+ALTER TABLE ONLY posts ALTER COLUMN id SET DEFAULT nextval('posts_id_seq'::regclass);
